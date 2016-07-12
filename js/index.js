741c5ff00a04b7f6b416c8e7b6529e1c5b1bf1b5
micah murakami <mickmura21@gmail.com>
2016-07-12 11:49:37 -1000

    hello

--- js/index.js
+++ js/index.js
@@ -4,31 +4,36 @@ var computerChoice = Math.random();
 if (computerChoice < 0.34) {
     computerChoice = "rock";
 } else if(computerChoice <= 0.67) {
-    computerChoice = "paper";
+    computerChoice =  "paper";
 } else {
-    computerChoice = "scissors";
+    computerChoice =  "scissors";
 } 
 
 console.log("Computer: " + computerChoice);
-document.getElementById("computer").innerHTML = "Computer: " + computerChoice;
-
+document.getElementById("computer").innerHTML = "Computer picks: " + computerChoice;
+document.getElementById("computer").innerHTML = "the game is a tie:";
+document.getElementById("computer").innerHTML = "computer picked " + computerChoice;
 var compare = function(choice1, choice2){
 
     if(choice1 === choice2) {
+        document.getElementById("winner").innerHTML = "The Game is a Tie: "; 
+        document.getElementById("computer").innerHTML = "Computer: " + computerChoice;
+        document.getElementById("computer").innerHTML = "computer picked " + computerChoice;
         return "The result is a tie!";
-        document.getElementById("winner").innerHTML = "The Game is a Tie"; 
-    } else if (choice1 === "rock") {
+    } else if (choice1 ===  "rock") {
 
-        if(choice2 === "scissors"){
-            return "rock wins";
-             document.getElementById(player1).innerHTML = 
+        if(choice2 ===  "scissors"){
+             document.getElementById("computer").innerHTML = "computer:" + computerChoice;
+             document.getElementById("winner").innerHTML = "The Game is a Tie: "; 
+             document.getElementById("computer").innerHTML =  "computer picked " + computerChoice; 
+            return  "rock wins";
         }else{
-            return "paper wins";   
+            return  "paper wins";   
         }
     }
-    else if(choice1 === "paper") {
+    else if(choice1 ===  "paper") {
 
-        if(choice2 === "rock") {
+        if(choice2 ===  "rock") {
             return "paper wins";
         } else {
             return "scissors wins";    
