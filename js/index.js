7e3855f1f5965f6693bab0468f4ca2b96364b8d1
micah murakami <mickmura21@gmail.com>
2016-07-14 10:48:33 -1000

    human change\

--- js/index.js
+++ js/index.js
@@ -15,61 +15,57 @@ var compare = function(choice1, choice2){
 
     if(choice1 === choice2) {
         document.getElementById("winner").innerHTML = "The Game is a Tie: "; 
-        document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+        document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
         document.getElementById("buttons").innerHTML = "";
         return
     } else if (choice1 ===  "rock") {
         
         if(choice2 === "paper") {
             document.getElementById("winner").innerHTML = "Computer Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
             return
         } else {
-            document.getElementById("winner").innerHTML = "Human Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("winner").innerHTML = "player Wins "; 
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
             return
         }
     } else if ( choice1 === "scissors"){
         if (choice2 == "paper") {
-            document.getElementById("winner").innerHTML = "Human Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("winner").innerHTML = "player Wins "; 
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
             return
         } else {
             document.getElementById("winner").innerHTML = "Computer Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
         }
     } else if (choice1 == "rock"){
-
-        if (choice2 == "scissors"){
-            document.getElementById("winner").innerHTML = "Human Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+        if (choice2 == "scissors") {
+            document.getElementById("winner").innerHTML = "player Wins "; 
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
             return
          } else {
             document.getElementById("winner").innerHTML = "Computer Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
-
+            return
+         }  
     } else if (choice1 == "paper"){
-      if(choice2 == "scissors") {
+        }if(choice2 == "scissors") {
 
         document.getElementById("winner").innerHTML = "Computer Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
             return
         } else {
-            document.getElementById("winner").innerHTML = "Human Wins "; 
-            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "Human: "+ choice1;
+            document.getElementById("winner").innerHTML = "player Wins "; 
+            document.getElementById("computer").innerHTML = "Computer: " + choice2 + "  Player: "+ choice1;
             document.getElementById("buttons").innerHTML = "";
-
-  
-
-
-
+        }
 
 
 
